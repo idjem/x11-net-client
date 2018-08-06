bd101905bba58b6320b6662cbf514a9454bf6a52
François Leurent <fleurent@ivsweb.com>
2018-08-06 14:10:24 +0000

    Send x&y always

--- webClient.js
+++ webClient.js
@@ -6,30 +6,24 @@ const throttle = require('mout/function/throttle');
 class Client {
 
   constructor(dom, client, client_key) {
-
-    var ns = client_key ? `mouse:${client_key}` : 'mouse';
-  
-    var handelMouseDown = ({clientX, clientY, which}) => {
+    var ns  = client_key ? `mouse:${client_key}` : 'mouse';
+    var pos = (e) => {
       var {x, y, width, height} = dom.getClientRects()[0];
-      var pos_x = (clientX - x + 1) / width;
-      var pos_y = (clientY - y + 1) / height;
-      client.send(ns, 'click', which, pos_x, pos_y);
+      return [(e.clientX - x + 1) / width, (e.clientY - y + 1) / height];
     };
 
-    var handelMouseRightClick = ({clientX, clientY, preventDefault}) => {
-      preventDefault();
-      var {x, y, width, height} = dom.getClientRects()[0];
-      var pos_x = (clientX - x + 1) / width;
-      var pos_y = (clientY - y + 1) / height;
-      client.send(ns, 'click', 3, x, y);
+    var handelMouseDown = (e) => {
+      client.send(ns, 'click', e.which, ...pos(e));
+    };
+
+    var handelMouseRightClick = (e) => {
+      e.preventDefault();
+      client.send(ns, 'click', 3, ...pos(e));
       return false;
     }
 
-    var handelMouseMove = throttle(({clientX, clientY}) => {
-      var {x, y, width, height} = dom.getClientRects()[0];
-      var pos_x = (clientX - x + 1) / width;
-      var pos_y = (clientY - y + 1) / height;
-      client.send(ns, 'move', pos_x, pos_y);
+    var handelMouseMove = throttle((e) => {
+      client.send(ns, 'move', ...pos(e));
     }, 100);
 
     dom.addEventListener('mousemove', handelMouseMove, false);
